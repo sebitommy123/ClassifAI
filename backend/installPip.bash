98f5bab47b74e0e67d981a21e1cb03faaf5a018d
sebitommy123 <sebi.szafir@hotmail.com>
2021-11-30 18:09:43 -0800

    quickfix

--- backend/installPip.bash
+++ backend/installPip.bash
@@ -1,11 +1,11 @@
 python3 -m pip install opendatasets --upgrade --user
-&& python3 -m pip install seaborn --upgrade --user
-&& python3 -m pip install opencv-python-headless --upgrade --user
-&& python3 -m pip install numpy --upgrade --user
-&& python3 -m pip install pandas --upgrade --user
-&& python3 -m pip install matplotlib --upgrade --user
-&& python3 -m pip install scikit-learn --upgrade --user
-&& python3 -m pip install tensorflow --upgrade --user
-&& python3 -m pip install tensorflow_hub --upgrade --user
-&& python3 -m pip install flask --upgrade --user
-&& python3 -m pip install flask_cors --upgrade --user
+python3 -m pip install seaborn --upgrade --user
+python3 -m pip install opencv-python-headless --upgrade --user
+python3 -m pip install numpy --upgrade --user
+python3 -m pip install pandas --upgrade --user
+python3 -m pip install matplotlib --upgrade --user
+python3 -m pip install scikit-learn --upgrade --user
+python3 -m pip install tensorflow --upgrade --user
+python3 -m pip install tensorflow_hub --upgrade --user
+python3 -m pip install flask --upgrade --user
+python3 -m pip install flask_cors --upgrade --user
